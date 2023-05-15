4ebf6fb3726a6ab6554cdc0d96d2f4446f5937fd
Minwoo Gwak <minwoogwak99@gmail.com>
2023-05-15 16:35:10 +0900

    changed a tag to Link tag for faster rendering

--- src/components/hamburger/Hamburger.js
+++ src/components/hamburger/Hamburger.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./hamburger.css";
 
 function Hamburger(props) {
@@ -6,14 +7,14 @@ function Hamburger(props) {
     props.isTopMenu && (
       <div className="hamburger">
         <div className="hamburgerEl">ooo님 안녕하세요</div>
-        <a className="hamburgerEl" href="/items">
+        <Link className="hamburgerEl" to="/items">
           <i className="fa-solid fa-gift"></i>
           상품리스트 페이지
-        </a>
-        <a className="hamburgerEl" href="/bookmarked">
+        </Link>
+        <Link className="hamburgerEl" to="/bookmarked">
           <i className="fa-regular fa-star-half-stroke"></i>
           북마크 페이지
-        </a>
+        </Link>
       </div>
     )
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import { Link } from "react-router-dom";
 
 import "./header.css";
 import logoImage from "../../logo.svg";
@@ -14,9 +15,9 @@ function Header() {
   return (
     <div className="header">
       <div className="left">
-        <a href="/">
+        <Link to="/">
           <img src={logoImage} alt="" className="logoImage" />
-        </a>
+        </Link>
         <div className="logoTitle">5 Stars Shopping</div>
       </div>
       <div className="right" onClick={menuToggle}>
